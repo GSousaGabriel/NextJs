1860998755aac8933ccb8690f9ede4a180ba0f4f
Gabriel Sousa <gabriel.gs@sidi.org.br>
2023-07-19 16:32:00 -0300

    started with nextJs

---
+++ nextjs-first-ap/pages/portfolio/[portfolioid].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+const PortfolioSingle = () => {
+    const params= useRouter()
+
+    return (
+        <h1>A single portfolio</h1>
+    )
+}
+
+export default PortfolioSingle
---
+++ nextjs-first-ap/pages/portfolio/index.tsx
@@ -0,0 +1,9 @@
+import classes from '@/styles/Home.module.css'
+
+const PortfolioPage = () => {
+    return (
+        <h1 className={classes.userWelcome}>Portfolio user 23</h1>
+    )
+}
+
+export default PortfolioPage
